448a310c54963602c0172b30db2643e2f9964c9a
Onur Yılmaz <Onurylmz.41@outlook.com>
2025-01-11 14:16:59 +0300

    add StarRating component and update main rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 import { useState } from "react";
 
@@ -66,137 +67,181 @@ const selected_movie_list = [
 const getAvarage = (array) =>
   array.reduce((sum, value) => sum + value, 0) / array.length;
 
-function App() {
+export default function App() {
   const [movies, setMovies] = useState(movie_list);
   const [selectedMovie, setSelectedMovie] = useState(selected_movie_list);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  const avgRating = getAvarage(selectedMovie.map((movie) => movie.rating));
-  const avgDuration = getAvarage(selectedMovie.map((movie) => movie.duration));
   return (
     <>
-      <nav className="bg-primary text-white p-2">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-4">Movie App</div>
-            <div className="col-4">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Film arayınız..."
-              />
-            </div>
-            <div className="col-4 text-end">
-              <strong>5</strong> kayıt bulundu.
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <main className="container">
+      <Nav>
+        <Logo />
+        <Search />
+        <NavSearchResult movies={movies} />
+      </Nav>
+      <Main>
         <div className="row mt-2">
           <div className="col-md-9">
-            <div className="movie-list">
-              <button
-                className="btn btn-sm btn-outline-primary mb-2"
-                onClick={() => setIsOpen1(!isOpen1)}
-              >
-                {isOpen1 ? (
-                  <i className="bi bi-chevron-up"></i>
-                ) : (
-                  <i className="bi bi-chevron-down"></i>
-                )}
-              </button>
-              {isOpen1 && (
-                <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
-                  {movies.map((movie) => (
-                    <div className="col mb-2" key={movie.Id}>
-                      <div className="card">
-                        <img
-                          src={movie.Poster}
-                          alt={movie.Title}
-                          className="card-img-top"
-                        />
-                        <div className="card-body">
-                          <h6 className="card-title">{movie.Title}</h6>
-                          <div>
-                            <i class="bi bi-calendar2-date me-1"></i>
-                            <span>{movie.Year}</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
+            <ListContainer>
+              <MovieList movies={movies} />
+            </ListContainer>
           </div>
           <div className="col-md-3">
-            <div className="movie_list">
-              <button
-                className="btn btn-sm btn-outline-primary mb-2"
-                onClick={() => setIsOpen2(!isOpen2)}
-              >
-                {isOpen2 ? (
-                  <i className="bi bi-chevron-up"></i>
-                ) : (
-                  <i className="bi bi-chevron-down"></i>
-                )}
-              </button>
-              {isOpen2 && (
-                <>
-                  <div className="card mb-2">
-                    <div className="card-body">
-                      <h5>Listeye [{selectedMovie.length}] film ekledi.</h5>
-                      <div className="d-flex justify-content-between">
-                        <p>
-                          <i className="bi bi-star-fill text-warning me-1"></i>
-                          <span>{avgRating.toFixed(2)}</span>
-                        </p>
-                        <p>
-                          <i className="bi bi-hourglass-split text-info me-1"></i>
-                          <span>{avgDuration.toFixed(2)} dk</span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  {selectedMovie.map((movie) => (
-                    <div className="card mb-2" key={movie.Id}>
-                      <div className="row">
-                        <div className="col-4">
-                          <img
-                            src={movie.Poster}
-                            alt={movie.Title}
-                            className="img-fluid rounded-start"
-                          />
-                        </div>
-                        <div className="col-8">
-                          <div className="card-body">
-                            <h6 className="card-title">{movie.Title}</h6>
-                            <div className="d-flex justify-content-between">
-                              <p>
-                                <i className="bi bi-star-fill text-warning me-1"></i>
-                                <span>{movie.rating}</span>
-                              </p>
-                              <p>
-                                <i className="bi bi-hourglass text-info me-1"></i>
-                                <span>{movie.duration} dk</span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </>
-              )}
-            </div>
+            <ListContainer>
+              <>
+                <MyListSummary selectedMovie={selectedMovie} />
+                <MyMovieList selectedMovie={selectedMovie} />
+              </>
+            </ListContainer>
           </div>
         </div>
-      </main>
+      </Main>
     </>
   );
 }
 
-export default App;
+function Nav({ children }) {
+  return (
+    <nav className="bg-primary text-white p-2">
+      <div className="container">
+        <div className="row align-items-center">{children}</div>
+      </div>
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="col-4">
+      <i className="bi bi-camera-reels me-2"></i>
+      Movie App
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div className="col-4">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Film arayınız..."
+      />
+    </div>
+  );
+}
+
+function NavSearchResult({ movies }) {
+  return (
+    <div className="col-4 text-end">
+      <strong>{movies.length}</strong> kayıt bulundu.
+    </div>
+  );
+}
+
+function Main({ children }) {
+  return <main className="container">{children}</main>;
+}
+
+function ListContainer({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="movie-list">
+      <button
+        className="btn btn-sm btn-outline-primary mb-2"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? (
+          <i className="bi bi-chevron-up"></i>
+        ) : (
+          <i className="bi bi-chevron-down"></i>
+        )}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+function MovieList({ movies }) {
+  return (
+    <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
+      {movies.map((movie) => (
+        <Movie movie={movie} key={movie.Id} />
+      ))}
+    </div>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <div className="col mb-2">
+      <div className="card">
+        <img src={movie.Poster} alt={movie.Title} className="card-img-top" />
+        <div className="card-body">
+          <h6 className="card-title">{movie.Title}</h6>
+          <div>
+            <i class="bi bi-calendar2-date me-1"></i>
+            <span>{movie.Year}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MyListSummary({ selectedMovie }) {
+  const avgRating = getAvarage(selectedMovie.map((movie) => movie.rating));
+  const avgDuration = getAvarage(selectedMovie.map((movie) => movie.duration));
+  return (
+    <div className="card mb-2">
+      <div className="card-body">
+        <h5>Listeye [{selectedMovie.length}] film ekledi.</h5>
+        <div className="d-flex justify-content-between">
+          <p>
+            <i className="bi bi-star-fill text-warning me-1"></i>
+            <span>{avgRating.toFixed(2)}</span>
+          </p>
+          <p>
+            <i className="bi bi-hourglass-split text-info me-1"></i>
+            <span>{avgDuration.toFixed(2)} dk</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MyMovieList({ selectedMovie }) {
+  return selectedMovie.map((movie) => (
+    <MyListMovie key={movie.Id} movie={movie} />
+  ));
+}
+
+function MyListMovie({ movie }) {
+  return (
+    <div className="card mb-2">
+      <div className="row">
+        <div className="col-4">
+          <img
+            src={movie.Poster}
+            alt={movie.Title}
+            className="img-fluid rounded-start"
+          />
+        </div>
+        <div className="col-8">
+          <div className="card-body">
+            <h6 className="card-title">{movie.Title}</h6>
+            <div className="d-flex justify-content-between">
+              <p>
+                <i className="bi bi-star-fill text-warning me-1"></i>
+                <span>{movie.rating}</span>
+              </p>
+              <p>
+                <i className="bi bi-hourglass text-info me-1"></i>
+                <span>{movie.duration} dk</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+//import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
+import StarRating from "./StarRating.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
   </StrictMode>
 );
